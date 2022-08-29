3fe25ac562fe516d31365a65125c25914a271622
tfnLAB <tfnlab@gmail.com>
2022-08-29 15:39:08 -0700

    update

--- src/components/AccountDetails/AccountDetails.jsx
+++ src/components/AccountDetails/AccountDetails.jsx
@@ -8,16 +8,14 @@ const AccountDetails = ({ accountAddress, accountBalance }) => {
         <div class="card col-md-12" >
           <div class="card-body">
             <p className="lead">
-                Metagascar Contract Address : 0xf286e4955557361a7d245358b0d47a3f5c735b2e
+            The Metaverse is open, and the rocketships of explorers have been dispatched! The first MetaIsland to be discovered by the Alpha Explorer "Meta" has been dubbed Metagascar. Here you will find a thriving community of unique MetaHumans who reside in their equally unique MetaHomes. On the island of Metagascar you will find various facet's of social activity including friendship, dating, and of course what island would be complete without messages in a bottle!
+
+
             </p>
-          </div>
-        </div>
-        <div class="card col-md-12" >
-          <div class="card-body">
-            <p className="lead">Account address :</p>
-            <h4>{accountAddress}</h4>
-            <p className="lead">Account balance :</p>
-            <h4>{accountBalance} ETH</h4>
+            <p className="lead">
+              Welcome to Metagasacar Metaverse            
+            </p>
+
           </div>
         </div>
       </div>
